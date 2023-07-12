7f2b5d3929e86368ae888bb054a216f22b101482
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-07-12 05:50:10 -0300

    ✔️test(index): aplica tratamento de exceção | Parte 06
    - Implementa try/catch para exemplificar lançamento, captura e tratamento de exceção

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { Categoria } from "./modules/catalogo/domain/categoria.entity";
 
-let categoria = Categoria.criar({nome:'teste'});
-console.log(categoria);
+try {
+    let categoria = Categoria.criar({nome:'cama'});
+    console.log(categoria);
+} 
+catch (error:any) {
+    console.log(error.message);
+}
+finally {
+    console.log('Ação que deve ser executada em caso de sucesso e em caso de exceção');
+}
