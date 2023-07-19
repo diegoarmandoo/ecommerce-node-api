41844caab213449fc4f2c57fe479d3c7e844bb40
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-07-18 23:27:17 -0300

    ✔️test(index): aplica tratamento de exceção refinado | Parte 07
    - Insere verificação do tipo de exceção no bloco catch.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Categoria } from "./modules/catalogo/domain/categoria.entity";
+import { DomainException } from "./shared/domain/domain.exception";
 
 try {
-    let categoria = Categoria.criar({nome:'cama'});
+    let categoria: Categoria;
+    categoria = Categoria.criar({nome:'ma'});
     console.log(categoria);
 } 
 catch (error:any) {
-    console.log(error.message);
+    if (error instanceof DomainException) {
+        console.log(error.message);
+    }
 }
 finally {
     console.log('Ação que deve ser executada em caso de sucesso e em caso de exceção');
