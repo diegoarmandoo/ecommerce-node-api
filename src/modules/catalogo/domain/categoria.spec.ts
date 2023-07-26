8ffe5afcfe13ace0f90d04ee210d2c349166b5f0
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-07-26 00:38:00 -0300

    ✔️test(categoria): aplica testes de unidade automatizados | Parte 09
    - Implementa o conjunto de testes para o caso de uso recuperar categoria da entidade de domínio com os casos de teste de exceção e padrão baseados na história de usuário
    - Implementa exceção específica para ID  de entidade que são UUIDs inválidos

--- src/modules/catalogo/domain/categoria.spec.ts
+++ src/modules/catalogo/domain/categoria.spec.ts
@@ -1,11 +1,12 @@
 import { describe, expect, test } from 'vitest';
-import { CriarCategoriaProps } from './categoria.types';
+import { CriarCategoriaProps, RecuperarCategoriaProps } from './categoria.types';
 import { Categoria } from './categoria.entity';
 import { NomeCategoriaTamanhoMaximoInvalido, NomeCategoriaTamanhoMinimoInvalido } from './categoria.exception';
+import { IDEntityUUIDInvalid } from '../../../shared/domain/domain.exception';
 
 //Suite de Testes de Unidade - Entidade de Domínio
 //Usando a descrição, você pode definir como um conjunto de testes ou benchmarks relacionados
-describe('Entidade de Domínio: Categoria', () => {
+describe('Entidade de Domínio: Criar Categoria', () => {
 
     //Teste define um conjunto de expectativas relacionadas. 
     test('Deve Criar Uma Categoria Válida', async () => {
@@ -45,6 +46,69 @@ describe('Entidade de Domínio: Categoria', () => {
 
         //Quando (When) e Então (Then)
         expect(() => Categoria.criar(categoriaNomeInvalido))
+            .toThrowError(NomeCategoriaTamanhoMaximoInvalido);
+
+    });
+
+});
+
+describe('Entidade de Domínio: Recupear Categoria', () => {
+
+    test('Deve Recuperar Uma Categoria Válida', async () => {
+
+        //Dado (Given)
+        const categoriaValida: RecuperarCategoriaProps = {
+            id: '5edbc79d-b724-4a39-a29b-0bfb2386920a',
+            nome: 'cama'
+        };
+
+        //Quando (When) e Então (Then)
+        expect(Categoria.recuperar(categoriaValida))
+            .to.be.instanceof(Categoria);
+
+    });
+
+    test('Não Deve Recuperar Categoria Com ID Inválido (UUID Inválido)', async () => {
+
+        //Dado (Given)
+        //Nome menor que três caracteres
+        const categoriaIdInvalido: RecuperarCategoriaProps = {
+            id: '1234',
+            nome: 'cama'
+        };
+
+        //Quando (When) e Então (Then)
+        expect(() => Categoria.recuperar(categoriaIdInvalido))
+            .toThrowError(IDEntityUUIDInvalid);
+
+    });
+
+    test('Não Deve Recuperar Categoria Com Nome Inválido (Tamanho Mínimo)', async () => {
+
+        //Dado (Given)
+        //Nome menor que três caracteres
+        const categoriaNomeInvalido: RecuperarCategoriaProps = {
+            id: '5edbc79d-b724-4a39-a29b-0bfb2386920a',
+            nome: 'ma'
+        };
+
+        //Quando (When) e Então (Then)
+        expect(() => Categoria.recuperar(categoriaNomeInvalido))
+            .toThrowError(NomeCategoriaTamanhoMinimoInvalido);
+
+    });
+
+    test('Não Deve Recuperar Categoria Com Nome Inválido (Tamanho Máximo)', async () => {
+
+        //Dado (Given)
+        //Nome maior que 50 caracteres
+        const categoriaNomeInvalido: RecuperarCategoriaProps = {
+            id: '5edbc79d-b724-4a39-a29b-0bfb2386920a',
+            nome: '123456789123456789123456789123456789123456789123456'
+        };
+
+        //Quando (When) e Então (Then)
+        expect(() => Categoria.recuperar(categoriaNomeInvalido))
             .toThrowError(NomeCategoriaTamanhoMaximoInvalido);
 
     });
--- src/shared/domain/domain.exception.ts
+++ src/shared/domain/domain.exception.ts
@@ -1,10 +1,21 @@
 class DomainException extends Error {
-    constructor(message:string) {
+    constructor(message:string = '⚠️ Exceção de domínio genérica') {
         super(message);
         this.name = 'DomainException';
-        this.message = 'Exceção de domínio genérica';
+        this.message = message;
         Error.captureStackTrace(this, this.constructor)
     }
 }
 
-export { DomainException }
+class IDEntityUUIDInvalid extends DomainException {
+    public constructor(message:string = '⚠️ O ID da entidade é um UUID inválido.') {
+        super(message);
+        this.name = 'IDEntityUUIDInvalid'
+        this.message = message;
+    }
+}
+
+export { 
+    DomainException,
+    IDEntityUUIDInvalid
+}
