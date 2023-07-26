b94da359113a723c2438fd6b7605b63d592862df
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-07-26 00:39:21 -0300

    ✔️test(index): verifica a criação e recuperação de forma manual | Parte 09
    - Testa manualmente a criação e recuperação de entidades do tipo Categoria

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import { Categoria } from "./modules/catalogo/domain/categoria.entity";
+import { RecuperarCategoriaProps } from "./modules/catalogo/domain/categoria.types";
 import { DomainException } from "./shared/domain/domain.exception";
 
 try {
+
+    ///////////////////
+    //Criar Categoria//
+    ///////////////////
+
     let categoria: Categoria;
-    categoria = Categoria.criar({nome:'ma'});
+    categoria = Categoria.criar({nome:'mesa'});
     console.log(categoria);
+
+    ///////////////////////
+    //Recuperar Categoria//
+    ///////////////////////
+
+    let propsCategoria: RecuperarCategoriaProps = {
+        id:'6ad12850-abe4-49fe-967e-ab915cce9b3a',
+        nome: 'cama'
+    };
+    let categoria2: Categoria = Categoria.recuperar(propsCategoria);
+    console.log(categoria2);
+
 } 
 catch (error:any) {
     if (error instanceof DomainException) {
+        console.log(error.message);
+    }
+    else {
         console.log(error.message);
     }
 }
