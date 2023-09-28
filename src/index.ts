70b4c08d14ac728a4db886167f97eab9c7f13591
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-09-28 05:00:14 -0300

    ✔️test(index): teste manual com repositório prisma | Parte 19
    - Testa manualmente os métodos implementados no repositório prisma do objeto de domínio categoria

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,80 @@
 import { Categoria } from '@modules/catalogo/domain/categoria/categoria.entity';
+import { CategoriaPrismaRepository } from '@modules/catalogo/infra/database/categoria.prisma.repository';
 import { PrismaClient } from '@prisma/client';
 import { DomainException } from '@shared/domain/domain.exception';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info'],
+    errorFormat: 'pretty'
+});
 
 async function main() {
     
-    ///////////////////
-	//Criar Categoria//
-	///////////////////
-
-	/*let categoria: Categoria;
-	categoria = Categoria.criar({nome:'mesa'});
-
-    ////////////////////////////////
-	//Persistir Categoria no Banco//
-	////////////////////////////////
-		
-    await prisma.categoria.create({
-        data: {
-            id: categoria.id,
-            nome: categoria.nome
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado');
         }
-    });*/
+    );
+
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
     ////////////////////////////////
-    //Atualizar Categoria no Banco//
+    //Recuperar Categoria por UUID//
     ////////////////////////////////
     
-    const categoriaRecuperada = await prisma.categoria.update({
-        where: { id: "3ddc3641-f163-47cd-b1ab-11f7ff3206fc" },
-        data: { nome: 'banho' },
-    })
+    const categoriaRecuperada: Categoria | null = await categoriaRepo.recuperarPorUuid("7061d559-ab25-4182-98ce-170afdf2acd2");
 
+    console.log(categoriaRecuperada);
+
+    /////////////////////////////////
+    //Recuperar Todas as Categorias//
+    /////////////////////////////////
+    
+    //const todasCategorias: Array<Categoria> = await categoriaRepo.recuperarTodos();
+
+    //console.log(todasCategorias);
+
+    ////////////////////////////////
+    //Verifica se Existe Categoria//
+    ////////////////////////////////
+    
+    //const existeCategoria: boolean = await categoriaRepo.existe("7061d559-ab25-4182-98ce-170afdf2acd2");
+
+    //console.log(existeCategoria);
 
     /////////////////////
-	//Listar Categorias//
-	/////////////////////
+    //Inserir Categoria//
+    /////////////////////
+    
+    //const categoria: Categoria = Categoria.criar({
+    //    nome:'Sala e Quarto'
+    //});     
 
-    const ListaCategorias = await prisma.categoria.findMany();
-    console.log(ListaCategorias);
+    //const categoriaInserida = await categoriaRepo.inserir(categoria);
+
+    //console.log(categoriaInserida);
+
+    ///////////////////////
+    //Atualizar Categoria//
+    ///////////////////////
+    
+    //const categoria: Categoria = Categoria.recuperar({
+    //    id: "96a7f212-e01d-4de7-8abc-70cabbc898fd",
+    //    nome: "Banho"
+    //});     
+
+    //const atualizouCategoria: boolean = await categoriaRepo.atualizar(categoria.id,categoria);
+
+    //console.log(atualizouCategoria)
+
+    /////////////////////
+    //Deletar Categoria//
+    /////////////////////
+    
+    //const categoriaDeletada: boolean = await categoriaRepo.deletar("120a3d76-9ca6-4880-a1d6-d34685e1f6f8");
+    
+    //console.log(categoriaDeletada);
+
 }
 
 main()
